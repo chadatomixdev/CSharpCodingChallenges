1d87d8fa623dbd65f96b3932d5e02834bab2adaf
Chad Bonthuys <chad@atomixdev.com>
2019-12-17 09:23:00 +0000

    Sock Merchant - Hacker Rank Challenge

---
+++ CSharpCodingChallenges/HackerRankWarmupChallenges/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HackerRankWarmupChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSocks = 9;
+            int[] socks = { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+            int result = SockMerchant.sockMerchant(numberOfSocks, socks);
+
+            Console.WriteLine($"Matching pairs of socks { result }");
+        }
+    }
+}
---
+++ CSharpCodingChallenges/HackerRankWarmupChallenges/SockMerchant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRankWarmupChallenges
+{
+    public static class SockMerchant
+    {
+        // Complete the sockMerchant function below.
+        public static int sockMerchant(int n, int[] ar)
+        {
+            var pairs = 0;
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = i+1; j < n; j++)
+            //    {
+            //        if (ar[i] == ar[j])
+            //            pairs++;
+            //    }
+            //}
+
+            var combinations = ar.SelectMany((element, index) => ar.Skip(index + 1), (sock1, sock2) => Tuple.Create(sock1, sock2));
+
+            foreach (var combination in combinations)
+            {
+                if (combination.Item1 == combination.Item2)
+                    pairs++;
+            }
+
+            return pairs;
+        }
+    }
+}
